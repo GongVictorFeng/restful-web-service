bf4f685f4a8aa39169614762858606b41920ddae
victor <gfeng005@uottawa.ca>
2025-05-06 11:40:25 -0400

    Initialized project

---
+++ src/main/java/com/restful_web_service/simple_social_media_rest/SimpleSocialMediaRestApplication.java
@@ -0,0 +1,13 @@
+package com.restful_web_service.simple_social_media_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialMediaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSocialMediaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restful_web_service/simple_social_media_rest/SimpleSocialMediaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful_web_service.simple_social_media_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSocialMediaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
