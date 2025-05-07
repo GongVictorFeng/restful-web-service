2fbc6cdc5033ae0ca698923b75a47451a9feff9e
victor <gfeng005@uottawa.ca>
2025-05-07 10:01:11 -0400

    Created a User controller and Rest Api for retrieve all users and get user by id.

---
+++ src/main/java/com/restful_web_service/simple_social_media_rest/user/UserResource.java
@@ -0,0 +1,27 @@
+package com.restful_web_service.simple_social_media_rest.user;
+
+import com.restful_web_service.simple_social_media_rest.user.dao.UserDaoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private final UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+}
