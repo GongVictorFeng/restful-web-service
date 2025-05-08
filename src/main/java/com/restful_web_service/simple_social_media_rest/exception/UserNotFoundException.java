8bc5eb1cbddd1658ab7685775cf10a4e58200dee
victor <gfeng005@uottawa.ca>
2025-05-08 09:56:14 -0400

    created customized exception for user not found

---
+++ src/main/java/com/restful_web_service/simple_social_media_rest/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.restful_web_service.simple_social_media_rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
