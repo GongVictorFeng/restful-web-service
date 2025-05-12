40f6fa9cd62a3ab23655f4c697e8884c22ff90ac
victor <gfeng005@uottawa.ca>
2025-05-12 10:41:03 -0400

    added the feature of Internationalization - i18n.

--- src/main/java/com/restful_web_service/simple_social_media_rest/user/UserResource.java
+++ src/main/java/com/restful_web_service/simple_social_media_rest/user/UserResource.java
@@ -3,19 +3,23 @@ package com.restful_web_service.simple_social_media_rest.user;
 import com.restful_web_service.simple_social_media_rest.exception.UserNotFoundException;
 import com.restful_web_service.simple_social_media_rest.user.dao.UserDaoService;
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserResource {
     private final UserDaoService userDaoService;
-
-    public UserResource(UserDaoService userDaoService) {
+    private final MessageSource messageSource;
+    public UserResource(UserDaoService userDaoService, MessageSource messageSource) {
         this.userDaoService = userDaoService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/users")
@@ -45,5 +49,11 @@ public class UserResource {
     @DeleteMapping("users/{id}")
     public void deleteUserById(@PathVariable int id) {
         userDaoService.deleteOne(id);
+    }
+
+    @GetMapping("users/greeting-internationalization")
+    public String greeting() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
